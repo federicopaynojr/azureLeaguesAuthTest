9d7ba2d898622731a4518cbcc8c8e82a98a9be32
sprintforcefepa <fepa@splitmedialabs.com>
2013-01-09 21:20:07 +0800

    added passport authentication

--- server.js
+++ server.js
@@ -1,4 +1,55 @@
-// require('./api/LeaguesApi.js');
+var port = process.env.port || 8080;
+var express = require('express'),
+	passport = require('passport'),
+	TwitterStrategy = require('passport-twitter').Strategy;
+
+var users = [];
+
+passport.use(new TwitterStrategy({
+		consumerKey: 'twitter-app-consumer-key',
+		cnsumerSecret: 'twitter-app-consumer-secret',
+		callbackURL: 'http://test.passport-twitter.com:3000/auth/twitter/callback'
+	},
+	function(token, tokenSecret, profile, done) {
+		var user = users[profile.id] ||
+					(users[profile.id] = { id: profile.id, name: profile.username});
+		done(null, user);
+	}
+));
+
+application = express();
+
+application.configure(function() {
+	application.use(express.bodyParser());
+	application.use(express.methodOverride());
+	application.use(express.cookieParser());
+	application.use(express.session({ secret: '498f993bbee4ae3a075eada02488464' }));
+	application.use(passport.initialize());
+	application.use(passport.session());
+	application.use(application.router);
+	application.use(express.errorHandler({ showStack: true, dumpExceptions: true }));
+	application.set('view engine', 'jade');
+});
+
+application.get('/auth/twitter', passport.authenticate('twitter'));
+
+application.get('/auth/twitter/callback',
+	passport.authenticate('twitter',
+		{ successRedirect: '/',
+		  failureRedirect: '/auth/twitter' }));
+pasport.serializeUser(function(user, done) {
+	done(null, user.id);
+});
+
+passport.deseriallizeUser(function(id, done) {
+	var user = users[id];
+	done(null, user);
+});
+
+application.listen(port);
+
+
+/* // require('./api/LeaguesApi.js');
 var port = process.env.port || 8080;
 var express = require("express");
 var server = express();
@@ -16,4 +67,4 @@ server.listen(port, function() {
 	console.log('test');
 	// console.log('%s listening at %s', server.name, server.url);
 	// console.log('net.Server.address() === ' + require('util').inspect(server.server.address()));
-});
+}); */
